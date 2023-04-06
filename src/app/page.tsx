43dbb1fc26ce5d3ebd22d6a2fc168267e0cfab47
youkwhd <lolywk@tutanota.com>
2023-04-06 20:54:12 +0700

    chore: update interfaces

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 "use client"
 
 import Header from "@/components/Header"
+import List from "@/components/List"
+
+import { motion } from "framer-motion"
+import Marquee from "@/components/Marquee"
 
 export default () => {
     return (
         <>
-            <Header />
+            <List />
         </>
     )
 }
