f8a9af2e58d76e91bcc7ab124c87dffcd46bc292
youkwhd <lolywk@tutanota.com>
2023-06-17 22:01:23 +0700

    feat: implement loader (bad)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,66 +3,84 @@
 import Footer from "@/components/Footer"
 import List from "@/components/List"
 import Marquee from "@/components/Marquee"
+import { useEffect, useState } from "react"
+
+const listContent = [
+    {
+        title: "duragest",
+        url: "https://github.com/youkwhd/duragest",
+        subtitle: {
+            year: 2023,
+            projectType: "Terminal Game"
+        }
+    },
+    {
+        title: "infinite scroll",
+        url: "https://www.npmjs.com/package/infinite-scroll.ts",
+        subtitle: {
+            year: 2023,
+            projectType: "TypeScript Library"
+        }
+    },
+    {
+        title: "twrap",
+        url: "https://github.com/youkwhd/twrap",
+        subtitle: {
+            year: 2022,
+            projectType: "Text Wrapper"
+        }
+    },
+    {
+        title: "libtmdb",
+        url: "https://www.themoviedb.org/documentation/api/wrappers-libraries",
+        subtitle: {
+            year: 2022,
+            projectType: "C Library"
+        }
+    },
+    {
+        title: "TUNE CWE 315",
+        url: "https://github.com/youkwhd/TUNE-CWE-315",
+        subtitle: {
+            year: 2023,
+            projectType: "Web Security"
+        }
+    },
+    {
+        title: "termstory",
+        url: "https://github.com/youkwhd/termstory",
+        subtitle: {
+            year: 2023,
+            projectType: "Game Generator"
+        }
+    },
+    {
+        title: "youkwhd",
+        url: "https://youkwhd.com",
+        subtitle: {
+            year: 2022,
+            projectType: "Web Development"
+        }
+    }
+]
 
 export default () => {
-    const listContent = [
-        { 
-            title: "duragest",
-            url: "https://github.com/youkwhd/duragest",
-            subtitle: {
-                year: 2023,
-                projectType: "Terminal Game"
-            }
-        },
-        {
-            title: "infinite scroll",
-            url: "https://www.npmjs.com/package/infinite-scroll.ts",
-            subtitle: {
-                year: 2023,
-                projectType: "TypeScript Library"
-            }
-        },
-        {
-            title: "twrap",
-            url: "https://github.com/youkwhd/twrap",
-            subtitle: {
-                year: 2022,
-                projectType: "Text Wrapper"
-            }
-        },
-        {
-            title: "libtmdb",
-            url: "https://www.themoviedb.org/documentation/api/wrappers-libraries",
-            subtitle: {
-                year: 2022,
-                projectType: "C Library"
-            }
-        },
-        {
-            title: "TUNE CWE 315",
-            url: "https://github.com/youkwhd/TUNE-CWE-315",
-            subtitle: {
-                year: 2023,
-                projectType: "Web Security"
-            }
-        },
-        {
-            title: "termstory",
-            url: "https://github.com/youkwhd/termstory",
-            subtitle: {
-                year: 2023,
-                projectType: "Game Generator"
-            }
-        },
-        {
-            title: "youkwhd",
-            url: "https://youkwhd.com",
-            subtitle: {
-                year: 2022,
-                projectType: "Web Development"
-            }
-        }
-    ]
+    const [loading, setLoading] = useState(true)
+
+    /* Not really waiting for everything to finish.
+     * Since <List /> and <Marquee /> takes time to actually build.
+     */
+    useEffect(() => {
+        setLoading((state) => !state)
+    }, [])
+
+    if (loading)
+        return (
+            <div className="w-full h-full bg-__primary flex items-center justify-center flex-col transition-opacity duration-75 ease-out">
+                <h1 className="text-4xl">:: youkwhd</h1>
+                <span className="text-xl">Loading</span>
+            </div>
+        )
 
     return (
         <>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+        colors: {
+            "__primary": "#3b76ac",
+        }
+    },
   },
   plugins: [],
 }
